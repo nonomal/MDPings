503ca2e1cab446cac07c2a7946d55686d4ff6537
icylian <red4357@gmail.com>
2024-12-11 19:27:55 +0900

    Update version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sekusarisu.mdpings"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.3"
+        versionCode = 10
+        versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
